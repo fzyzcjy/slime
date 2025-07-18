12ba6c8bc363abbc31264424df8546c9f6fd4d8f
fzyzcjy <ch271828n@outlook.com>
2025-07-18 21:49:05 +0800

    more

---
+++ slime/rollout/sglang_example_v2.py
@@ -0,0 +1,50 @@
+from slime.utils.types import Sample
+
+async def generate_rollout_async(args, rollout_id: int, data_buffer) -> list[list[Sample]]:
+    data = []
+    while len(data) < target_data_size:
+        pendings = _submit_generate_tasks(min_size=target_data_size-state.remaining_batch_size)
+
+        # wait for the generation to finish
+        done, state.pendings = await asyncio.wait(state.pendings, return_when=asyncio.FIRST_COMPLETED)
+        for task in done:
+            group: list[Sample] = task.result()
+
+            if do_print:
+                print(
+                    f"First rollout sample: {[group[0].prompt + group[0].response]}, label: {group[0].label}, reward: {group[0].reward}",
+                    flush=True,
+                )
+                do_print = False
+
+            assert len(group) == args.n_samples_per_prompt
+            if dynamic_filter is not None and not dynamic_filter(args, group):
+                state.remaining_batch_size -= 1
+                continue
+
+            # add the samples to the data
+            # NOTE: here we have not stored all the unused samples back to the data buffer.
+            if len(data) < target_data_size:
+                data.append(group)
+                pbar.update(args.n_samples_per_prompt)
+
+def _submit_generate_tasks(min_size: int):
+    pendings = []
+    # this part is copied from genereate_rollout_async
+    while len(pendings) < min_size:
+        # get samples from the buffer and submit the generation requests.
+        samples = data_buffer.get_samples(args.over_sampling_batch_size)
+
+        # this part is copied from State.submit_generate_tasks
+        for group in samples:
+            pendings.add(
+                asyncio.create_task(
+                    # submit a group of samples as a single task.
+                    generate_and_rm_group(
+                        self.args,
+                        group,
+                        sampling_params=self.sampling_params.copy(),
+                        evaluation=False,
+                    )
+                )
+            )
