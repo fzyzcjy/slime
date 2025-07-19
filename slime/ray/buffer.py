ad078910682e5c43952db739860c91159c1fabd9
fzyzcjy <ch271828n@outlook.com>
2025-07-19 19:49:51 +0800

    more

--- slime/ray/buffer.py
+++ slime/ray/buffer.py
@@ -304,6 +304,9 @@ class Buffer:
 
 def _dump_rollout_data(dir_out, data, rollout_id: int, evaluation: bool):
     phase = 'eval' if evaluation else 'train'
+    path = Path(dir_out) / "rollout" / f"{rollout_id}_{phase}.parquet"
+    path.parent.mkdir(parents=True, exist_ok=True)
+
     df = pl.DataFrame([sample.to_dict() for sample in data])
     df = df.with_columns(rollout=pl.lit(rollout_id), phase=pl.lit(phase))
-    df.write_parquet(Path(dir_out) / "rollout" / f"{rollout_id}_{phase}.parquet")
+    df.write_parquet(path)
