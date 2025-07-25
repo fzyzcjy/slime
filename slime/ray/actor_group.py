836117a388a0e597baad0eaa5e0dfed22b1a8955
fzyzcjy <ch271828n@outlook.com>
2025-07-25 16:46:12 +0800

    fix
    (cherry picked from commit cef6fd72326df4001f1459d2fad3cc1fa664b963)

--- slime/ray/actor_group.py
+++ slime/ray/actor_group.py
@@ -104,9 +104,9 @@ class RayTrainGroup:
     def get_rollout_data(self, rollout_id):
         ray.get([actor.get_rollout_data.remote(rollout_id) for actor in self._actor_handlers])
 
-    def async_train(self, rollout_id):
+    def async_train(self, rollout_id, rollout_data_ref):
         """Do one rollout training"""
-        return [actor.train.remote(rollout_id) for actor in self._actor_handlers]
+        return [actor.train.remote(rollout_id, rollout_data_ref) for actor in self._actor_handlers]
 
     def async_eval(self, rollout_id, rollout_data_ref):
         """Evaluate the model"""
